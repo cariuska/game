0bb23fef7db4dbceaaf8da59d7edadb3f383f42f
Thiago Cariuska <tcariuska@gmail.com>
2021-02-26 21:59:16 -0300

    Melhorias

--- public/keyboard-listener.js
+++ public/keyboard-listener.js
@@ -1,7 +1,12 @@
 export default function createKeyboardListener(document) {
 
     const state = {
-        observers: []
+        observers: [],
+        playerId: null
+    }
+
+    function registerPlayerId(playerId){
+        state.playerId = playerId
     }
 
     function subscribe(observerFunction) {
@@ -20,7 +25,7 @@ export default function createKeyboardListener(document) {
         const keyPressed = event.key
         
         const command = {
-            playerId: 'player1',
+            playerId: state.playerId,
             keyPressed
         }
 
@@ -29,6 +34,7 @@ export default function createKeyboardListener(document) {
     }
 
     return {
-        subscribe
+        subscribe,
+        registerPlayerId
     }
 }
--- server.js
+++ server.js
@@ -10,15 +10,25 @@ const sockets = new Server(server)
 app.use(express.static('public'))
 
 const game = createGame()            
-game.addPlayer({ playerId: 'player1', playerX: 0, playerY: 0 })
-game.addFruit({ fruitId: 'fruit1', fruitX: 5, fruitY: 5 })
-game.addFruit({ fruitId: 'fruit2', fruitX: 8, fruitY: 7 })
+//game.addPlayer({ playerId: 'player1', playerX: 0, playerY: 0 })
+//game.addFruit({ fruitId: 'fruit1', fruitX: 5, fruitY: 5 })
+
+game.subscribe((command) => {
+    sockets.emit(command.type, command)
+})
 
 sockets.on('connection', (socket) => {
     const playerId = socket.id
 
-    console.log(playerId)
+    game.addPlayer({ playerId: playerId })
+
+    socket.emit('setup', game.state)
+    
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId: playerId })
+    })
 })
+
 
 server.listen(8080, ()=> {
     console.log('> Server listening on port: 8080')
